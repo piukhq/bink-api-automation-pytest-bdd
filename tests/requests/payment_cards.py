a0b92326fe297c2281f35748e67cbb2c647ee024
navyajames <njames@bink.com>
2020-07-09 07:48:23 +0100

    requests & payload add&link

---
+++ tests/requests/payment_cards.py
@@ -0,0 +1,38 @@
+import tests.api as api
+import json
+import jsonpath
+import time
+import logging
+from tests.payload.payment_cards.payment_card import PaymentCardDetails
+from tests.api.base import Endpoint
+
+
+class PaymentCards(Endpoint):
+
+    @staticmethod
+    def add_payment_card(token):
+        url = PaymentCards.get_url()
+        header = Endpoint.request_header(token)
+        payload = PaymentCardDetails.add_payment_card_payload()
+        return Endpoint.call(url, header, "POST", payload)
+
+    @staticmethod
+    def get_payment_card(token, payment_card_id):
+        url = PaymentCards.get_url(payment_card_id)
+        header = Endpoint.request_header(token)
+        response = Endpoint.call(url, header, "GET")
+        return response
+
+    @staticmethod
+    def delete_payment_card(token, payment_card_id):
+        url = PaymentCards.get_url(payment_card_id)
+        header = Endpoint.request_header(token)
+        response = Endpoint.call(url, header, "DELETE")
+        return response
+
+    @staticmethod
+    def get_url(payment_card_id=None):
+        if payment_card_id is None:
+            return Endpoint.BASE_URL + api.ENDPOINT_PAYMENT_CARDS
+        else:
+            return Endpoint.BASE_URL + api.ENDPOINT_PAYMENT_CARD.format(payment_card_id)
--- tests/step_definitions/test_membership_cards.py
+++ tests/step_definitions/test_membership_cards.py
@@ -14,6 +14,9 @@ from tests.requests.membership_cards import MembershipCards
 from tests.api.base import Endpoint
 from selenium.webdriver.support.ui import Select
 
+# from tests.helper.test_data_utils import TestDataUtils
+# import tests.helper.test_data_utils as test_data
+
 scenarios('membership_cards/')
 
 
@@ -39,9 +42,10 @@ def add_membership_card(merchant, login_user, context):
                Endpoint.TEST_DATA.membership_account_states.get('state_pending')
         logging.info('Response after POST is:\n ' + str(response.content))
 
-    except HTTPError as network_response:
-        assert network_response.response.status_code == 404 or 400
-        logging.error('Add Journey for ', merchant, ' failed due to HTTP error: {network_response')
+    except AssertionError as error:
+        # assert network_response.response.status_code == 404 or 400
+        # logging.error('Add Journey for ', merchant, ' failed due to error '+str(error))
+        raise Exception('Add Journey for ' + merchant + ' failed due to error ' + error.__str__())
 
 
 @when(parsers.parse('I perform POST request to add "{merchant}" membership card with "{invalid_data}"'))
@@ -62,6 +66,22 @@ def add_membership_card(merchant, login_user, context, invalid_data):
                       's with invalid data got failed due to HTTP error: {network_response')
 
 
+@when(parsers.parse('I perform POST request to add & auto link an existing "{merchant}" membership card'))
+def add_membership_card(merchant, login_user, context):
+    context['token'] = login_user.json().get('api_key')
+    response = MembershipCards.add_card_auto_link(login_user.json().get('api_key'), merchant)
+    response_json = response.json()
+    context['scheme_account_id'] = response_json.get('id')
+    try:
+        assert response.status_code == 201 \
+               and response_json['status']['state'] == \
+               Endpoint.TEST_DATA.membership_account_states.get('state_pending')
+
+    except HTTPError as network_response:
+        assert network_response.response.status_code == 404 or 400
+        logging.error(merchant, 's membership card Add Journey failed due to HTTP error: {network_response')
+
+
 @when(parsers.parse('I perform PATCH request to update "{merchant}" membership card'))
 def patch_request_to_update_membership_card_details(merchant, context):
     response = MembershipCards.patch_add_card(context['token'], context['scheme_account_id'], merchant)
@@ -70,29 +90,49 @@ def patch_request_to_update_membership_card_details(merchant, context):
         assert response.status_code == 200 \
                and response_json['status']['state'] == \
                Endpoint.TEST_DATA.membership_account_states.get('state_pending')
-        logging.info('Successfully performed PATCH on '+merchant + 's membership card ')
+        logging.info('Successfully performed PATCH on ' + merchant + 's membership card ')
 
     except HTTPError as network_response:
         assert network_response.response.status_code == 404 or 400
-        logging.error('PATCH on', merchant, 's membership card failed due to HTTP error: {network_response')
+        logging.error(merchant, 's membership card PATCH failed due to HTTP error: {network_response')
 
 
+@when(parsers.parse('I perform GET request to verify "{merchant}" membership card is added to the wallet'))
 @when(parsers.parse(
     'I perform GET request to verify "{merchant}" membership card details got updated after a successful PATCH'))
-@when(parsers.parse('I perform GET request to verify "{merchant}" membership card is added to the wallet'))
 def verify_membership_card_is_added_to_wallet(merchant, context):
     response = MembershipCards.get_scheme_account(context['token'], context['scheme_account_id'])
     response_json = response.json()
     try:
         assert response.status_code == 200 \
-               and response_json['id'] == context['scheme_account_id'] \
-               and response_json['status']['state'] == \
-               Endpoint.TEST_DATA.membership_account_states.get('state_authorised')
-        logging.info('Response after GET: \n' + str(response.content))
+               and response_json['id'] == context['scheme_account_id']
+        # implement a new test data util class and re-write the following
+        # and response_json['card']['membership_id'] == test_data.get_card_number(merchant)
+        # and str(response_json['membership_plan']) == TestDataUtils.get_membership_plan_id(merchant) \
+        # and response_json['status']['state'] == TestDataUtils.membership_account_state_authorised \
+
+        logging.info(merchant, 's membership card is added /updated successfully: \n' + str(response.content))
 
     except HTTPError as network_response:
         assert network_response.response.status_code == 404 or 400
         logging.error('POST/PATCH on ', merchant, 's membership card failed due to HTTP error: {network_response')
+
+
+@when(parsers.parse(
+    'I perform GET request to verify "{merchant}" membership card is added & linked successfully in the wallet'))
+def verify_membership_card_is_add_and_linked(merchant, context, add_payment_card):
+    response = MembershipCards.get_scheme_account_auto_link(context['token'])
+    response_json = response.json()
+    try:
+        assert response.status_code == 200 \
+               and response_json[0]['id'] == context['scheme_account_id'] \
+               and response_json[0]['status']['state'] == \
+               Endpoint.TEST_DATA.membership_account_states.get('state_authorised')
+        logging.info('Membership card is added and auto linked : \n' + str(response.content))
+
+    except HTTPError as network_response:
+        assert network_response.response.status_code == 404 or 400
+        logging.error('Add and AutoLink of', merchant, 's Membership card failed due to HTTP error: {network_response')
 
 
 @when(parsers.parse(
@@ -103,14 +143,32 @@ def verify_membership_card_is_added_to_wallet(merchant, context):
     try:
         assert response.status_code == 200 \
                and response_json['id'] == context['scheme_account_id'] \
-               and response_json['status']['state'] ==\
-               Endpoint.TEST_DATA.membership_account_states. get('state_failed')
+               and response_json['status']['state'] == \
+               Endpoint.TEST_DATA.membership_account_states.get('state_authorised')
+
         logging.info('Response after GET (invalid data) is: \n ' + str(response.content))
 
     except HTTPError as network_response:
         assert network_response.response.status_code == 404 or 400
         logging.error('Unable to add membership card for', merchant, 'due to HTTP error: {network_response')
 
+@when(parsers.parse(
+    'I perform GET request to view balance for recently added membership card'))
+def verify_membership_card_is_added_to_wallet(context):
+    response = MembershipCards.get_membership_card_balance(context['token'])
+    response_json = response.json()
+    logging.info(' GET request to view balance for recently added membership card: \n ' + str(response.content))
+
+    try:
+        # Assert other balance realted field checks
+        assert response.status_code == 200 \
+               and response_json[0]['id'] == context['scheme_account_id'] \
+               and response_json[0]['status']['state'] == \
+               Endpoint.TEST_DATA.membership_account_states.get('state_authorised')
+
+    except HTTPError as network_response:
+        assert network_response.response.status_code == 404 or 400
+        logging.error('Unable to add membership card for', merchant, 'due to HTTP error: {network_response')
 
 @when(parsers.parse('I perform POST request to create a "{merchant}" membership account with enrol details'))
 def enrol_membership_account(merchant, register_user, context, test_email):
@@ -149,6 +207,8 @@ def verify_membership_account_join_date_card_number_and_merchant_identifier_popu
     current_date = time.strftime("%d %b %Y").lstrip('0')
     if str(link_date).__contains__(current_date):
         logging.info("Link date in Django (" + link_date + ") is close to current date "
-                                                 "(" + current_date + time.strftime(", %I:%M %p").lower() + ")")
-    logging.info('Merchant Identifier in Django is: ' + driver.find_element_by_name('schemeaccountcredentialanswer_set-1-answer').
+                                                           "(" + current_date + time.strftime(
+            ", %I:%M %p").lower() + ")")
+    logging.info('Merchant Identifier in Django is: ' + driver.find_element_by_name(
+        'schemeaccountcredentialanswer_set-1-answer').
                  get_attribute('value'))
