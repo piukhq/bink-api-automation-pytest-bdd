d46340d31deba87e97376a32143c7564eef162d6
navyajames <njames@bink.com>
2020-07-23 16:10:11 +0100

    Updated conftest, constants , & pipfile

--- tests/helpers/constants.py
+++ tests/helpers/constants.py
@@ -20,6 +20,11 @@ ID = 'id'
 PASSWORD = 'password'
 LAST_NAME = 'last_name'
 
+""" Membership Card's Status.State"""
+AUTHORIZED = 'state_authorised'
+PENDING = 'state_pending'
+FAILED = 'state_failed'
+
 """Base Path of expected membership plans and 
 membership plans used for comparison in json_diff"""
 EXPECTED_MEMBERSHIP_PLANS_PATH = 'tests_resources/test_data/membership_plan'
