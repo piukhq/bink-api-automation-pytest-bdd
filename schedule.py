6dfb8f48c2b165804f990881fd179f70c13e4692
Chris Pressland <mail@cpressland.io>
2020-09-16 17:38:54 +0100

    Minor tweaks

--- schedule.py
+++ schedule.py
@@ -1,4 +1,6 @@
 import os
+import string
+import random
 import requests
 import subprocess
 from datetime import datetime
@@ -10,9 +12,9 @@ env = os.getenv("ENV", "dev")
 mode = os.getenv("MODE", "daily")
 blob_storage_dsn = os.getenv("BLOB_STORAGE_DSN")
 
-teams_webhook_qa = 'https://outlook.office.com/webhook/bf220ac8-d509-474f-a568-148982784d19@a6e2367a-92ea-4e5a-b565-723830bcc095/IncomingWebhook/5d25733db5994811b6ee4049ef74713e/48aca6b1-4d56-4a15-bc92-8aa9d97300df' # noqa
-teams_webhook_alerts_qa = 'https://outlook.office.com/webhook/bf220ac8-d509-474f-a568-148982784d19@a6e2367a-92ea-4e5a-b565-723830bcc095/IncomingWebhook/0856493823a1484b9adfa37c942d2da4/48aca6b1-4d56-4a15-bc92-8aa9d97300df' # noqa
-teams_webhook_alerts_production = 'https://outlook.office.com/webhook/bf220ac8-d509-474f-a568-148982784d19@a6e2367a-92ea-4e5a-b565-723830bcc095/IncomingWebhook/7ae4116d366e4e5a92a65d9135a0664d/48aca6b1-4d56-4a15-bc92-8aa9d97300df' # noqa
+teams_webhook_qa = "https://outlook.office.com/webhook/bf220ac8-d509-474f-a568-148982784d19@a6e2367a-92ea-4e5a-b565-723830bcc095/IncomingWebhook/5d25733db5994811b6ee4049ef74713e/48aca6b1-4d56-4a15-bc92-8aa9d97300df"  # noqa
+teams_webhook_alerts_qa = "https://outlook.office.com/webhook/bf220ac8-d509-474f-a568-148982784d19@a6e2367a-92ea-4e5a-b565-723830bcc095/IncomingWebhook/0856493823a1484b9adfa37c942d2da4/48aca6b1-4d56-4a15-bc92-8aa9d97300df"  # noqa
+teams_webhook_alerts_production = "https://outlook.office.com/webhook/bf220ac8-d509-474f-a568-148982784d19@a6e2367a-92ea-4e5a-b565-723830bcc095/IncomingWebhook/7ae4116d366e4e5a92a65d9135a0664d/48aca6b1-4d56-4a15-bc92-8aa9d97300df"  # noqa
 
 config = {
     "dev": {
@@ -25,11 +27,9 @@ config = {
             "-m",
             "dev",
             "--channel",
-            "barclays"
+            "barclays",
         ],
-        "daily": {
-            "cron": "0 22 * * *",
-        },
+        "daily": {"cron": "5 22 * * *",},
     },
     "staging": {
         "command": [
@@ -45,9 +45,7 @@ config = {
             "--channel",
             "barclays",
         ],
-        "daily": {
-            "cron": "0 22 * * *",
-        },
+        "daily": {"cron": "5 22 * * *",},
     },
     "preprod": {
         "command": [
@@ -62,12 +60,8 @@ config = {
             "--channel",
             "barclays",
         ],
-        "daily": {
-            "cron": "0 22 * * *",
-        },
-        "continuous": {
-            "cron": "*/10 * * * *",
-        }
+        "daily": {"cron": "5 22 * * *",},
+        "continuous": {"cron": "*/10 * * * *",},
     },
     "prod": {
         "command": [
@@ -82,18 +76,13 @@ config = {
             "--channel",
             "barclays",
         ],
-        "daily": {
-            "cron": "0 22 * * *",
-        },
-        "continuous": {
-            "cron": "*/10 * * * *",
-        },
+        "daily": {"cron": "5 22 * * *",},
+        "continuous": {"cron": "*/10 * * * *",},
     },
 }
 
 
 def run_test():
-    print('---- Running Test ----')
     process = subprocess.run(config[env]["command"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     print(process.stdout.decode())
     if process.returncode == 0:
@@ -101,10 +90,10 @@ def run_test():
     else:
         status = "Failure"
     url = upload("report.html")
-    if env == 'prod':
-        if status == 'Success':
+    if env == "prod":
+        if status == "Success":
             post(teams_webhook_alerts_qa, status, url)
-        if status == 'Failure':
+        if status == "Failure":
             post(teams_webhook_alerts_production, status, url)
             post(teams_webhook_alerts_qa, status, url)
     else:
@@ -115,7 +104,7 @@ def upload(filename):
     blob = BlobClient.from_connection_string(
         conn_str=blob_storage_dsn,
         container_name="qareports",
-        blob_name=f"{datetime.now().strftime('%Y%m%d-%H%M')}.html",
+        blob_name=f"{datetime.now().strftime('%Y%m%d-%H%M')}-{''.join(random.choice(string.ascii_lowercase + string.digits) for _ in range(6))}.html",
     )
     with open(filename, "rb") as f:
         blob.upload_blob(f, content_settings=ContentSettings(content_type="text/html"))
