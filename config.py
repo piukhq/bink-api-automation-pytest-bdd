e1d5240f8db963881e1f7aa111ad9f65f3c57329
navyajames <njames@bink.com>
2020-10-12 17:42:18 +0100

    SIT & OAT config changes & test data sheet

--- config.py
+++ config.py
@@ -1,46 +1,51 @@
+from os import environ
 from tests_resources.test_data import testdata_dev
 from tests_resources.test_data import testdata_staging
 from tests_resources.test_data import testdata_prod
+from tests_resources.test_data import testdata_sit
+from tests_resources.test_data import testdata_oat
+
 
 
 class EnvironmentDetails:
-    def __init__(self, base_url, test_data, django_url):
+    def __init__(self, base_url, test_data):
         self.base_url = base_url
         self.test_data = test_data
-        self.django_url = django_url
 
 
 if "KUBERNETES_SERVICE_HOST" in environ:
     DEV = EnvironmentDetails(
         base_url="http://hermes-api",
         test_data=testdata_dev,
-        django_url="https://api.dev.gb.bink.com/admin/",
     )
     STAGING = EnvironmentDetails(
         base_url="http://hermes-api",
         test_data=testdata_staging,
-        django_url="https://api.staging.gb.bink.com/admin/",
     )
     PROD = EnvironmentDetails(
         base_url="http://hermes-api",
         test_data=testdata_prod,
-        django_url=" ",
     )
 else:
     DEV = EnvironmentDetails(
         base_url="https://api.dev.gb.bink.com",
         test_data=testdata_dev,
-        django_url="https://api.dev.gb.bink.com/admin/",
     )
     STAGING = EnvironmentDetails(
         base_url="https://api.staging.gb.bink.com",
         test_data=testdata_staging,
-        django_url="https://api.staging.gb.bink.com/admin/",
     )
     PROD = EnvironmentDetails(
         base_url="https://api.gb.bink.com",
         test_data=testdata_prod,
-        django_url=" ",
+    )
+    SIT = EnvironmentDetails(
+        base_url="https://api.sandbox.gb.bink.com",
+        test_data=testdata_sit,
+    )
+    OAT = EnvironmentDetails(
+        base_url="https://oat.sandbox.gb.bink.com",
+        test_data=testdata_oat,
     )
 
 
