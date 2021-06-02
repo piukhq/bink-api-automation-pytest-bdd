64bd72d976ab3cbf09d2606eb6e58e382cbfa3e4
rupalpatel <rpatel@bink.com>
2021-06-02 17:56:19 +0100

    flake8 resolved

--- tests/step_definitions/test_user_consent.py
+++ tests/step_definitions/test_user_consent.py
@@ -8,7 +8,6 @@ from pytest_bdd import (
 import logging
 
 import config
-from tests.conftest import expected_user_consent_json
 from tests.helpers.test_context import TestContext
 from tests.requests.service import CustomerAccount
 
@@ -31,6 +30,7 @@ def register_new_user(test_email, channel, env):
         TestContext.token = CustomerAccount.without_service_consent_banking_user(test_email)
         logging.info("User registration service token is: \n\n" + TestContext.token)
 
+
 @when(parsers.parse('I send a POST service request without the consent field'))
 def user_sent_empty_consent():
     response_consent = CustomerAccount.without_service_consent_bink_user(TestContext.token)
@@ -40,26 +40,22 @@ def user_sent_empty_consent():
 
 
 @when(parsers.parse('I send a POST service request without the consent key into request'))
-def user_sent_empty_consent():
+def user_sent_without_consent():
     response_consent = CustomerAccount.without_consent_key_bink_user(TestContext.token)
     TestContext.response = response_consent.json().__str__()
     TestContext.status_code = response_consent.status_code
     logging.info(f"User registration is not successful and bad request occured: \n\n {TestContext.response} \n")
 
+
 @when(parsers.parse('I send a POST service request without the consent mandatory field into request'))
-def user_sent_empty_consent():
+def user_sent_without_mandatory_field():
     response_consent = CustomerAccount.without_mandatory_consent_field(TestContext.token)
     TestContext.response = response_consent.json().__str__()
     TestContext.status_code = response_consent.status_code
     logging.info(f"User registration is not successful and bad request occured: \n\n {TestContext.response} \n")
 
+
 @then('I should receive an HTTP "<statuscode>" with a "<errordetail>"')
 def consent_error_message(statuscode, errordetail):
     assert (TestContext.status_code == int(statuscode) and
             TestContext.response == errordetail), ("User Registration_service consent is Invalid")
-
-
-
-
-
-
