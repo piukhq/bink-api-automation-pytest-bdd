7048bbf37d555654e5e72cb13821d69bf8743051
Bolabink <95231667+Bolabink@users.noreply.github.com>
2022-04-01 17:09:54 +0100

    Iceland Visa settlement transaction Matching (#31)

--- tests/requests/transaction_matching_payment_cards.py
+++ tests/requests/transaction_matching_payment_cards.py
@@ -27,6 +27,15 @@ class TransactionMatching(Endpoint):
         return response
 
     @staticmethod
+    def get_visa_settlement_file(mid):
+        url = TransactionMatching.get_visa_url()
+        header = TransactionMatching_Endpoint.request_header_visa()
+        payload = TransactionMatchingPaymentFileDetails.get_visa_settlement_data(mid)
+        response = Endpoint.call(url, header, "POST", payload)
+        print(json.dumps(payload, indent=4))
+        return response
+
+    @staticmethod
     def get_visa_spotting_merchant_auth_file(mid):
         url = TransactionMatching.get_visa_url()
         header = TransactionMatching_Endpoint.request_header_visa()
