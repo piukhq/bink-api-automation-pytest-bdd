eba745a4acc213facd42505448d14151eba14417
Thenuja Viknarajah <thenuja_1@outlook.com>
2022-07-28 16:26:22 +0100

    update url

--- report-render.sh
+++ report-render.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-bolb_name=$(date +%s | awk '{ print strftime("%Y%m%d-%H%M", $1);  }')-$(echo $RANDOM | md5sum | head -c 6)
-url="https://binkuksouthstaging.blob.core.windows.net/qareports/${bolb_name}.html"
+bolb_name=$(date +%s | awk '{ print strftime("%Y%m%d-%H%M", $1);  }')-$(echo $RANDOM | md5sum | head -c 6).html
+url="https://binkuksouthstaging.blob.core.windows.net/test/${bolb_name}.html"
 webhook="https://hellobink.webhook.office.com/webhookb2/bf220ac8-d509-474f-a568-148982784d19@a6e2367a-92ea-4e5a-b565-723830bcc095/IncomingWebhook/05a1d268dc4c44ee84b8765d9b6fa6fd/e69fd5a7-8b6c-4ac5-8df0-c88c77df0a12"
 
 # look for report.html generated during pytest run
