d9ec05a4d2a8f752f0cf8f5f57e9bc5559ac0a21
Thenuja Viknarajah <thenuja_1@outlook.com>
2022-08-01 11:37:56 +0100

    change webhook from test to QA alerts

--- report-render.sh
+++ report-render.sh
@@ -2,7 +2,6 @@
 
 bolb_name=$(date +%s | awk '{ print strftime("%Y%m%d-%H%M", $1);  }')-$(echo $RANDOM | md5sum | head -c 6).html
 url="https://binkuksouthstaging.blob.core.windows.net/test/${bolb_name}"
-webhook="https://hellobink.webhook.office.com/webhookb2/bf220ac8-d509-474f-a568-148982784d19@a6e2367a-92ea-4e5a-b565-723830bcc095/IncomingWebhook/05a1d268dc4c44ee84b8765d9b6fa6fd/e69fd5a7-8b6c-4ac5-8df0-c88c77df0a12"
 
 # look for report.html generated during pytest run
 while [ ! -f /tmp/report.html ]; do
@@ -25,7 +24,7 @@ else
 fi
 
 # POST to teams the output of pytest run
-curl -H 'Content-Type: application/json' -d '{"@type": "MessageCard", "@context": "http://schema.org/extensions", "themeColor": "'"$themeColor"'", "summary": "Staging - Merchant API V1.2 Test Results", "Sections": [{"activityTitle": "Staging - Merchant API V1.2 Test Results", "facts": [{"name": "Status", "value": "'"$status"'"}, {"name": "URL", "value": "'"$url"'"}]}]}' $webhook
+curl -H 'Content-Type: application/json' -d '{"@type": "MessageCard", "@context": "http://schema.org/extensions", "themeColor": "'"$themeColor"'", "summary": "Staging - Merchant API V1.2 Test Results", "Sections": [{"activityTitle": "Staging - Merchant API V1.2 Test Results", "facts": [{"name": "Status", "value": "'"$status"'"}, {"name": "URL", "value": "'"$url"'"}]}]}' $TEAMS_WEBHOOK
 
 
 curl  -XPOST localhost:4191/shutdown
