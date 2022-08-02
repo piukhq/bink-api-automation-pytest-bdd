dceb04b8e5d435aef2d2b57d9c210f19d96dd140
Thenuja Viknarajah <thenuja_1@outlook.com>
2022-08-02 16:20:30 +0100

    logging info

--- tests/payload/payment_cards/payment_card.py
+++ tests/payload/payment_cards/payment_card.py
@@ -58,8 +58,8 @@ class PaymentCardDetails:
     @staticmethod
     def get_card(card_provider):
         faker = Faker()
-        print("PaymentCard", PaymentCardTestData.get_data(card_provider))
-        print("Constants", constants.HASH)
+        logging.info(PaymentCardTestData.get_data(card_provider))
+        logging.info(constants.HASH)
         TestContext.payment_card_hash = PaymentCardTestData.get_data(card_provider).get(constants.HASH) + str(
             faker.random_int()
         )
