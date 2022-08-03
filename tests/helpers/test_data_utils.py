5d5a07799da4842a448e20ee01c4e27b1e079c40
Thenuja Viknarajah <thenuja_1@outlook.com>
2022-08-03 08:30:29 +0100

    more logging

--- tests/helpers/test_data_utils.py
+++ tests/helpers/test_data_utils.py
@@ -1,4 +1,5 @@
 import config
+from settings import logger
 
 
 class TestDataUtils:
@@ -9,3 +10,7 @@ class TestDataUtils:
         # TestDataUtils.TEST_DATA = EnvironmentDetails.return_current_env(env).test_data
 
         TestDataUtils.TEST_DATA = getattr(config, env.upper()).test_data
+        
+        logger.info("ENV: ", env)
+        logger.info("ATTRIBUTES: ", getattr(config, env.upper()))
+        logger.info("TEST_DATA: ", TestDataUtils.TEST_DATA)
