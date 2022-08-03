d7ed3a6fb1aa775a9b8805b6c5c27215b2c8a736
Thenuja Viknarajah <thenuja_1@outlook.com>
2022-08-03 08:35:14 +0100

    format typo

--- tests/helpers/test_data_utils.py
+++ tests/helpers/test_data_utils.py
@@ -9,8 +9,8 @@ class TestDataUtils:
     def set_test_data(env):
         # TestDataUtils.TEST_DATA = EnvironmentDetails.return_current_env(env).test_data
 
-        logger.info("ENV: ", env)
-        logger.info("ATTRIBUTES: ", getattr(config, env.upper()))
-        logger.info("TEST_DATA: ", TestDataUtils.TEST_DATA)
+        logger.info("ENV: "+ env)
+        logger.info("ATTRIBUTES: "+ getattr(config, env.upper()))
+        logger.info("TEST_DATA: "+ TestDataUtils.TEST_DATA)
 
         TestDataUtils.TEST_DATA = getattr(config, env.upper()).test_data
