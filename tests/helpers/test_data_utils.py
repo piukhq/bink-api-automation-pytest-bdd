a281e698f5374af8d0977f28f186a20a60cb4279
Thenuja Viknarajah <thenuja_1@outlook.com>
2022-08-05 11:59:36 +0100

    remove experimental log lines

--- tests/helpers/test_data_utils.py
+++ tests/helpers/test_data_utils.py
@@ -1,5 +1,4 @@
 import config
-from settings import logger
 
 
 class TestDataUtils:
@@ -8,8 +7,5 @@ class TestDataUtils:
     @staticmethod
     def set_test_data(env):
         # TestDataUtils.TEST_DATA = EnvironmentDetails.return_current_env(env).test_data
-        TestDataUtils.TEST_DATA = getattr(config, env.upper()).test_data
 
-        logger.info("ENV: " + env)
-        logger.info("ATTRIBUTES: " + str(getattr(config, env.upper())))
-        logger.info("TEST_DATA: " + str(TestDataUtils.TEST_DATA.master_payment_card))
+        TestDataUtils.TEST_DATA = getattr(config, env.upper()).test_data
