f8af693c4a38efe0463a202a3c71c358da53a9f9
srikalyanikotha <skotha@bink.com>
2022-08-18 15:49:42 +0100

    Update membership plan for ASOS and balance change on the card.

--- tests_resources/test_data/testdata_staging.py
+++ tests_resources/test_data/testdata_staging.py
@@ -256,7 +256,7 @@ trenette_invalid_data = {
 asos_membership_card = {
     "card_num": "ASOS3279362771",
     "email": "pytest+220722@bink.com",
-    "points": 37.0,
+    "points": 39.88,
     "prefix": "£",
     "currency": "GBP",
     "transaction_currency": "GBP",
