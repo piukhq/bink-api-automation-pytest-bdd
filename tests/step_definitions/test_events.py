ba6f8e842db295f7bc2aae835db8eeb70e274f19
RupalPatelj <rpatel@bink.com>
2023-06-23 12:42:40 +0100

    Added Join event

--- tests/step_definitions/test_events.py
+++ tests/step_definitions/test_events.py
@@ -17,7 +17,6 @@ scenarios("events/")
 
 @then(parsers.parse("I verify that {journey_type} event"))
 def verify_loyalty_card_into_event_database(journey_type):
-    # TestContext.extid = TestContext.external_id[user]
     time.sleep(5)
     logging.info(TestDataUtils.TEST_DATA.event_type.get(journey_type))
     event_record = QuerySnowstorm.fetch_event(TestDataUtils.TEST_DATA.event_type.get(journey_type),
@@ -37,7 +36,6 @@ def verify_loyalty_card_into_event_database(journey_type):
 
 @then(parsers.parse("I verify {journey_type} loyalty scheme event is created for {user}"))
 def verify_scheme_into_event_database(journey_type, user):
-    # TestContext.extid = TestContext.external_id[user]
     time.sleep(5)
     logging.info(TestDataUtils.TEST_DATA.event_type.get(journey_type))
     event_record = QuerySnowstorm.fetch_event(TestDataUtils.TEST_DATA.event_type.get(journey_type),
@@ -54,6 +52,12 @@ def verify_scheme_into_event_database(journey_type, user):
     assert event_record.event_type == TestDataUtils.TEST_DATA.event_type.get(journey_type), 'eventype didnt match'
     assert event_record.json["email"] == TestContext.user_email, 'user email didnt match'
     assert event_record.json["scheme_account_id"] == TestContext.current_scheme_account_id, 'scheme id didnt match'
+
+    if journey_type == "lc_join_success":
+        assert event_record.json["consents"][0]["slug"] == \
+               TestDataUtils.TEST_DATA.event_info.get(constants.SLUG), "consent slug didn't match"
+        assert event_record.json["consents"][0]["response"] == \
+               TestDataUtils.TEST_DATA.event_info.get(constants.RESPONSE), "consent response didn't match"
     return event_record
 
 
@@ -77,3 +81,13 @@ def verify_data_stored_in_db(journey_type, merchant):
 @when(parsers.parse('I perform POST request to add and auth "{merchant}" membershipcard with "{credentials}"'))
 def verify_add_auth_membership(merchant, credentials):
     test_membership_cards_multi_wallet.add_auth_membership_card(merchant, credentials)
+
+
+@when(parsers.parse('I perform POST request to create a "{merchant}" membershipcard account with enrol credentials'))
+def i_perform_post_enrol_membership_card(merchant, test_email, env, channel):
+    test_membership_cards.enrol_membership_account(merchant, test_email, env, channel)
+
+
+@when(parsers.parse('I perform GET request to verify the "{merchant}" membershipcard account is created'))
+def i_perform_get_request(merchant):
+    test_membership_cards.verify_membership_card_is_created(merchant)
--- tests_resources/features/events/user_create_event.feature
+++ tests_resources/features/events/user_create_event.feature
@@ -1,11 +1,11 @@
 # Created by rupalpatel at 09/06/2023
-@user_created @events
+@user_created @events @sanity
 Feature: Add and authorise a loyalty card
   As a DM Administrator
   I want to see an Event logged when a user is created
   so that this Business Event can be written to ClickHouse for validation
 
-  @user_created_event @sanity
+  @user_created_event
   Scenario: Verify event for user created
     Given I register with bink service in bink
     Then I verify that user_created event
